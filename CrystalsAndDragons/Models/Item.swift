776c2f22319d5553fdd18abd90d5c051231a451f
amanbayserkeev0377 <amanbayserkeev0377@gmail.com>
2026-03-24 12:35:09 +0600

    Add Room model

--- CrystalsAndDragons/Models/Item.swift
+++ CrystalsAndDragons/Models/Item.swift
@@ -1,8 +1,27 @@
-//
-//  Item.swift
-//  CrystalsAndDragons
-//
-//  Created by Aman Bayserkeev on 24/3/26.
-//
+enum ItemType {
+    case key
+    case chest
+    case torchlight
+    case food(restoreAmount: Int)
+    case sword
+    case gold(coins: Int)
+}
 
-import Foundation
+class Item {
+    let type: ItemType
+    let name: String
+    
+    init(type: ItemType, name: String) {
+        self.type = type
+        self.name = name
+    }
+    
+    var displayName: String {
+        switch type {
+        case .gold(let coins):
+            return "gold (\(coins) coins)"
+        default:
+            return name
+        }
+    }
+}
---
+++ CrystalsAndDragons/Models/Room.swift
@@ -0,0 +1,39 @@
+enum Direction: CaseIterable {
+    case north, south, east, west
+    
+    var opposite: Direction {
+        switch self {
+        case .north: return .south
+        case .south: return .north
+        case .east: return .west
+        case .west: return .east
+        }
+    }
+    
+    var displayName: String {
+        switch self {
+        case .north: return "N"
+        case .south: return "S"
+        case .east: return "E"
+        case .west: return "W"
+        }
+    }
+}
+
+class Room {
+    let x: Int
+    let y: Int
+    var doors: Set<Direction>
+    var items: [Item]
+    var isDark: Bool
+    var isLit: Bool
+    
+    init(x: Int, y: Int) {
+        self.x = x
+        self.y = y
+        self.doors = []
+        self.items = []
+        self.isDark = false
+        self.isLit = false
+    }
+}
